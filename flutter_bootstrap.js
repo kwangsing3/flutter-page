4aac6fe8bbe2965d2b466014d9948a35a074e986
kwangsing3 <kwangsing3@gmail.com>
2025-02-23 12:06:51 +0800

    fix base href to flutter-page

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1862854862"
+    serviceWorkerVersion: "1921014684"
   }
 });
